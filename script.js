4e94983d9e12a9ff3261722037e57f5ec130e047
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-01-12 09:23:12 -0300

    addapt

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let isEnglish = false
 
 const portuguese = {
     aboutMe: 'Sobre mim',
-    description: 'Verto é gay e gosta de dick.'
+    description: 'Sou Yan Isonel Pereira. tenho prática com tecnologia front-end e estou em busca da primeira oportunidade'
 }
 
 const english = {
     aboutMe: 'About me',
-    description: 'Verto is gay and like rola.'
+    description: 'I am Yan Isonel Pereira. I have practice with technology front-end and I have in search to the first opportunity '
 }
 
 const changeLanguage = () => {
     isEnglish = !isEnglish
     document.querySelector('.about-me').innerHTML = isEnglish ? english.aboutMe : portuguese.aboutMe
     document.querySelector('.about-me-text').innerHTML = isEnglish ? english.description : portuguese.description
+}
+
+const home = () => {
+ document.querySelector('.about-me-text').innerHTML = "This is the initial page."
+document.querySelector('.about-me-text').innerHTML+= img
 }
