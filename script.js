c749c53614277c1ac67406780340e5c25eb8a7e1
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-24 13:50:33 -0300

    studing js function

--- script.js
+++ script.js
@@ -30,6 +30,13 @@ const mostrarFuncao = () => {
   funcao =  elemento.style.display = "block";
 }
 
+function myFunc(theArr){
+    theArr[0] = 30 
+}
+const arr = [45]
+console.log(arr[0]);
+myFunc(arr);
+console.log(arr[0]);
 
 // fontes de orientação : 
 
