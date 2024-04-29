76f652415fb4bfc8b6efa2977d7a97329b9b1519
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-29 15:13:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,9 @@ const e2 = document.querySelector(".funcao");
   if( e1.style.display = "none" && e2.style.display == "block" ){
     e1.style.display = "block";
     e2.style.display = "none";
+  }else{
+    e2.style.display = "block"; // mostrar a area funcao
+    e1.style.display = "none";
   }
 
 
