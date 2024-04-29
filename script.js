1ca67d263e0628754fd921f855569b4c1529159f
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-29 13:58:12 -0300

    a

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const e2 = document.querySelector(".funcao");
     
  const r = () => {
     let radio = document.querySelector('manual-btn ')
-    radio.classList.toggle('activo')
+    radio.classList.toggle('manual-btn-activo')
  }
 
     }
