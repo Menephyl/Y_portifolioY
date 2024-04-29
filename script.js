be5a01928066d95482a0e308fc894229b461c0a4
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-04-29 08:22:52 -0300

    a

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const e2 = document.querySelector('oi');
 const toggleHome = () => {
 if((mostrarFuncao()  = true)) {
 e2.style.display ='none';
-// e.style.display ="block";
+e.style.display ="block";
  }
 
 }
