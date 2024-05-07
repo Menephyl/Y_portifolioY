15e9236015809e305169957549aa41fd20257ac0
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-05-06 22:03:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ revelar.reveal('.fadeToUp',{
 
 window.revelar = ScrollReveal({reset:true})
 // topo do projeto
-revelar.reveal('.fadeToUp-delay','fadeToUp-delay2', 'fadeToUp-delay3', 'fadeToUp-delay4','fadeToUp-delay5'{
+revelar.reveal('.fadeToUp-delay','fadeToUp-delay2', 'fadeToUp-delay3', 'fadeToUp-delay4',{
     duration:2000,
     distance:"90px",
     delay:500
