53d3235449db49f31a10cf0ffb603da4ecfe6fa3
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-05-16 00:30:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ document.getElementById('currentYear').textContent = new Date().getFullYear()
  3 criar funcao ao clicar com o mouse no radio 2, mostrar div 2 ( funcao)
  4- transição 1s entre essas divs.
 
-2 - transicionar eternamente a cada 5s entre esses dois blocos automaticamente 
-    1- set interval 
-   2 funcao if ( radio 1 ) mostrar (radio 2 ) e vice versa 
+2 - transicionar eternamente a cada 5s entre esses dois blocos automaticamente
+    1- set interval
+   2 funcao if ( radio 1 ) mostrar (radio 2 ) e vice versa
    3 nao dar comando de parada
 
 
 ÁREA HOME - TROCAR ENTRE OI E FUNCAO A CADA 3s
 
-1- pegar oi e funcao area  
+1- pegar oi e funcao area
 
-2-  definir troca manual entre essas duas caixas 
+2-  definir troca manual entre essas duas caixas
 
 3- definir troca interativa em cada 3s ou 2s entre cada área.
 
@@ -28,13 +28,13 @@ const e2 = document.querySelector(".funcao");
 
 
 const mostrarFunc = () => {
-    
+
   if( e1.style.display == "block"){
     e1.style.display = "none";
     e2.style.display = "block";
-  
+
   }else{
-    e1.style.display = "block";  
+    e1.style.display = "block";
     e2.style.display = "none";
   }
 }
@@ -48,8 +48,8 @@ revelar.reveal('.fadeToUp',{
     duration:2000,
     distance:"30px",
     origin:"bottom"
-   
-}) 
+
+})
 
 
 
@@ -58,8 +58,8 @@ window.revelar = ScrollReveal({reset:true})
 revelar.reveal('.fadeToUpFunc',{
     duration:2000,
     distance:"90px",
-   
-}) 
+
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -67,8 +67,8 @@ revelar.reveal('.fadeToUp-delay',{
     duration:2000,
     distance:"90px",
     delay:500
-}) 
-    
+})
+
 
 window.revelar = ScrollReveal({reset:true})
 // topo do projeto
@@ -76,7 +76,7 @@ revelar.reveal('.fadeToUp-delay2',{
     duration:2000,
     distance:"90px",
     delay:600
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -84,7 +84,7 @@ revelar.reveal('.fadeToUp-delay3',{
     duration:2000,
     distance:"90px",
     delay:700
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -92,7 +92,7 @@ revelar.reveal('.fadeToUp-delay4',{
     duration:2000,
     distance:"90px",
     delay:800
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -100,14 +100,14 @@ revelar.reveal('.fadeToUp-delay5',{
     duration:2000,
     distance:"90px",
     delay:900
-}) 
+})
 window.revelar = ScrollReveal({reset:true})
 
 revelar.reveal('.fadeToUp-delay6',{
     duration:2000,
     distance:"90px",
     delay:1000
-}) 
+})
 window.revelar = ScrollReveal({reset:true})
 
 revelar.reveal('.fadeToRight',{
@@ -115,7 +115,7 @@ revelar.reveal('.fadeToRight',{
     distance:"90px",
     // delay:500,
     origin:"left"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -124,7 +124,7 @@ revelar.reveal('.fadeToRight-2',{
     distance:"90px",
      delay:700,
     origin:"left"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -133,7 +133,7 @@ revelar.reveal('.fadeToRight-3',{
     distance:"90px",
      delay:900,
     origin:"left"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -142,7 +142,7 @@ revelar.reveal('.fadeToRight-4',{
     distance:"90px",
      delay:1100,
     origin:"left"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -151,7 +151,7 @@ revelar.reveal('.fadeToRight-5',{
     distance:"90px",
      delay:1200,
     origin:"left"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -160,7 +160,7 @@ revelar.reveal('.fadeToLeft',{
     distance:"90px",
      delay:500,
     origin:"right"
-}) 
+})
 
 window.revelar = ScrollReveal({reset:true})
 
@@ -204,7 +204,7 @@ revelar.reveal('.fadeToLeft-5',{
 
 
 const  nProject = document.getElementById('nProjects')
-let cont = 0 
+let cont = 0
 let tPro = setInterval(() => {
     cont+=1
 nProject.textContent=cont
@@ -216,7 +216,7 @@ if(cont===32){
 );
 
 const numero = document.getElementById('numero')
-let c = 0 
+let c = 0
 let t=setInterval(() => {
     c +=1
     numero.textContent=c
@@ -224,15 +224,15 @@ let t=setInterval(() => {
     if(c===99){
         clearInterval(t)
     }
-},80 
+},80
 );
 
 
 const nClients = document.getElementById("nClients")
 
-let cClients = 0 
+let cClients = 0
 let tClients = setInterval(()=>{
-    cClients+=1 
+    cClients+=1
     nClients.textContent=cClients
     if(c===52){
         clearInterval(tClients)
@@ -243,9 +243,9 @@ let tClients = setInterval(()=>{
 
 const nPartners = document.getElementById("nPartners")
 
-let cPartners = 0 
+let cPartners = 0
 let tPartners = setInterval(()=>{
-    cPartners+=1 
+    cPartners+=1
     nPartners.textContent=cPartners
     if(c===28){
         clearInterval(tPartners)
@@ -253,16 +253,16 @@ let tPartners = setInterval(()=>{
 
 },80
 );
- //  1- área education ok 
- // 2 ->  colocar no js para remover classe de  a estilo ativo e 
- // 2.1 pegar classe activo e remover 
- // 2.2 trocar classe de icone dentro do link de fa-minus (menos) pra fa-plus (mais) 
+ //  1- área education ok
+ // 2 ->  colocar no js para remover classe de  a estilo ativo e
+ // 2.1 pegar classe activo e remover
+ // 2.2 trocar classe de icone dentro do link de fa-minus (menos) pra fa-plus (mais)
 
  // 3->  nao exibir conteudo p dentro do panel body  ok
  // 3.1 colocar ao evento anterior feito. cada link corresponde a uma caixa com dois paragrafos.  ok
- //3.2 -> verificar se visivel, então esconder conteudo, se não, então mostrar ok 
+ //3.2 -> verificar se visivel, então esconder conteudo, se não, então mostrar ok
 
-/// issue on toggle fa-minus to fa.plus - adiconar e substituiar classe  de elemento com js 
+/// issue on toggle fa-minus to fa.plus - adiconar e substituiar classe  de elemento com js
 const courses = document.querySelector(".panel-body")
 const i_minus = document.querySelector('.fa-minus')
 const i_plus = document.querySelector('.fa-plus')
@@ -278,19 +278,19 @@ if(panelBody.style.display === 'flex'){
     if(btnSh.classList.contains('active')){
         btnSh.classList.remove('active')
         panelBody.style.display = 'none';
-      
-    }else{   
+
+    }else{
      btnSh.classList.add('active')
-     
-    }  
-    // toggle botao de mais para menos e vice versa 
-    if( ){
-          
+
     }
+    // toggle botao de mais para menos e vice versa
+    // if( ){
+
+    // }
 }else{
      panelBody.style.display = 'flex'
     btnSh.classList.add('active')
-    
+
 }
 })
 
@@ -300,10 +300,10 @@ btnSh2.addEventListener("click",() => {
         if(btnSh2.classList.contains('active')){
             btnSh2.classList.remove('active')
             panelBody2.style.display = 'none';
-        }else{   
+        }else{
          btnSh2.classList.add('active')
-         
-        }  
+
+        }
     }else{
          panelBody2.style.display = 'flex'
         btnSh2.classList.add('active')
@@ -317,10 +317,10 @@ btnSh2.addEventListener("click",() => {
             if(btnSh3.classList.contains('active')){
                 btnSh3.classList.remove('active')
                 panelBody3.style.display = 'none';
-            }else{   
+            }else{
              btnSh3.classList.add('active')
-             
-            }  
+
+            }
         }else{
              panelBody3.style.display = 'flex'
             btnSh3.classList.add('active')
@@ -332,7 +332,7 @@ btnSh2.addEventListener("click",() => {
     //     btnSh.classList.remove('active')
     // }else{     /// issue on toggle fa-minus to fa.plus
     //     btnSh.classList.add('active')
-    // }  
+    // }
 //     if(btnSh.classList.contains('.fa-minus')){
 //         btnSh.classList.remove('.fa-minus')
 //         btnSh.classList.add('.fa-plus')
@@ -403,8 +403,8 @@ const portuguese = {
     aboutMe: 'Sobre mim...',
     description: 'Sou Yan Isonel Pereira. Soluciono problemas do dia a dia com as tecnologias Front-End e estou em busca da minha primeira oportunidade. ',
     home:'Olá, eu sou o Yan'
-   
-    
+
+
 }
 
 const english = {
@@ -414,11 +414,11 @@ const english = {
 }
 
 
-// fontes de orientação : 
+// fontes de orientação :
 
 //elementoPlataformas.classList.add('ativo')
 // botao.addEventListener.toggle(`ativo`)
-/* 
+/*
 if(elementoPlataformas.classList.contains("ativo")){
     elementoPlataformas.classList.remove('ativo')
 }else{
@@ -432,11 +432,11 @@ if(elementoPlataformas.classList.contains("ativo")){
 // const rightAction = () => {
 //     const $image = document.querySelector('.seletor-da-imagem')
 //     imagem.style = 'margin-left: 200px' // os pixels deve ser do tamanho da imagem
-//   } 
+//   }
 // lembrando, o css da div pai tem que ter o overflow hidden, para quando a imagem filha der a margin, a imagem que foi trocada sumir
 // tem varias formas de fazer
 // tem uma mais facil:
- 
+
 // // botao para direita
 // const rightAction = () => {
 //   const $image = document.querySelector('.seletor-da-imagem')
@@ -446,9 +446,9 @@ if(elementoPlataformas.classList.contains("ativo")){
 // const images = ['link 1', 'link 2', 'link 3']
 
 // // ISSO DEFINE QUAL IMAGEM ESTA SENDO USADA
-// let imageIndex = 0 
- 
+// let imageIndex = 0
+
 // setInterval(() => {
 //     // troca a imgem aqui
-// //  }, 5000) 
+// //  }, 5000)
 */
