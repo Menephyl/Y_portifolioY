22a37c5d5669bf97622932ea572099cc382842da
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2024-05-17 03:27:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -280,8 +280,8 @@ const panelBody3 = document.querySelector(".panel-body3")
 
 
 
-const minus = document.querySelector('#iMenos')
 // criação das variaveis dos botões que se alterarão + pra menos 
+const minus = document.querySelector('#iMenos')
 let  el = document.createElement("i")
 el.classList = "fa-solid fa-plus";
 
