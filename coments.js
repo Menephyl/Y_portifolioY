ab3a86f404aa7793f95b1c1f89e2037785061ece
Yan Menephyl <87574460+Menephyl@users.noreply.github.com>
2025-09-26 15:32:09 -0300

    finisht sidebar

---
+++ coments.js
@@ -0,0 +1,142 @@
+// comentarios do que tinha do projeto no modo anterior 
+
+    // if(btnSh.classList.contains('active')){
+    //     btnSh.classList.remove('active')
+    // }else{     /// issue on toggle fa-minus to fa.plus
+    //     btnSh.classList.add('active')
+    // }
+//     if(btnSh.classList.contains('.fa-minus')){
+//         btnSh.classList.remove('.fa-minus')
+//         btnSh.classList.add('.fa-plus')
+//     }else{
+//         btnSh.classList.remove('.fa-plus')
+//         btnSh.classList.add('.fa-minus')
+//     }
+// }
+
+// }else{
+//     panelBody.style.display = 'block';
+
+    // if(btnSh.classList.contains('.fa-minus')){
+    //     btnSh.classList.remove('.fa-minus')
+    //     btnSh.classList.add('.fa-plus')
+    // }else{
+    //     btnSh.classList.remove('.fa-plus')
+    //     btnSh.classList.add('.fa-minus')
+//     }
+
+// const showCourse = document.querySelector('.showCourse')
+// showCourse.addEventListener("click",() =>{
+//     if(tileCourse.classList.contains('.active')){
+//         tileCourse.classList.remove(".active")
+//     }else{
+//         tileCourse.classList.add(".active")
+//     }
+// })
+
+
+// const closeCourse() =>{
+//     const titleCourse1 = document.querySelector('.fa-minus')
+
+
+
+// const changeLanguage = () => {
+//     isEnglish = !isEnglish
+//     document.querySelector('.about-me').innerHTML = isEnglish ? english.aboutMe : portuguese.aboutMe
+//     document.querySelector('.about-me-text').innerHTML = isEnglish ? english.description : portuguese.description
+// }
+/*/* Amante da tecnologia desde 1999. 
+
+
+/ fontes de orientação :
+
+//elementoPlataformas.classList.add('ativo')
+// botao.addEventListener.toggle(`ativo`)
+/*
+if(elementoPlataformas.classList.contains("ativo")){
+    elementoPlataformas.classList.remove('ativo')
+}else{
+    elementoPlataformas.classList.add('ativo')
+
+
+   */
+
+/*
+// botao para direita
+// const rightAction = () => {
+//     const $image = document.querySelector('.seletor-da-imagem')
+//     imagem.style = 'margin-left: 200px' // os pixels deve ser do tamanho da imagem
+//   }
+// lembrando, o css da div pai tem que ter o overflow hidden, para quando a imagem filha der a margin, a imagem que foi trocada sumir
+// tem varias formas de fazer
+// tem uma mais facil:
+
+// // botao para direita
+// const rightAction = () => {
+//   const $image = document.querySelector('.seletor-da-imagem')
+//   imagem.setAttribute('href', 'URL DA NOVA IMAGEM AQUI')
+// }
+// ai tu pode criar um array de links de imagens
+// const images = ['link 1', 'link 2', 'link 3']
+
+// // ISSO DEFINE QUAL IMAGEM ESTA SENDO USADA
+// let imageIndex = 0
+
+// setInterval(() => {
+//     // troca a imgem aqui
+// //  }, 5000)
+*/
+
+
+ /// issue on toggle fa-minus to fa.plus
+        // if(btnSh2.classList.contains('active')){
+            // btnSh2.classList.remove('active')
+            // panelBody2.style.display = 'none';
+        // }else{
+        //  btnSh2.classList.add('active')
+
+        // }
+
+
+        // minus.addEventListener("click",()=>{
+        // btnSh.appendChild(el,minus)
+        //     }
+        // )
+  
+            // )
+    //     if(btnSh.classList.contains('active')){
+          
+         
+
+    // }else{
+    //     el.addEventListener("click",()=>{
+    //    btnSh.classList.add('active')
+    //    btnSh.appendChild(minus,el)
+  
+    //  }
+    //  )
+    // }
+     
+
+// trocar icone para - ok 
+
+ //  1- área education ok
+ // 2 ->  colocar no js para remover classe de  a estilo ativo e ok
+ // 2.1 pegar classe activo e remover ok
+ // 2.2 trocar classe de icone dentro do link de fa-minus (menos) pra fa-plus (mais) ok
+
+ // 3->  nao exibir conteudo p dentro do panel body  ok
+ // 3.1 colocar ao evento anterior feito. cada link corresponde a uma caixa com dois paragrafos.  ok
+ //3.2 -> verificar se visivel, então esconder conteudo, se não, então mostrar ok
+
+/// issue on toggle fa-minus to fa.plus - adiconar e substituiar classe  de elemento co m js
+
+ // toggle botao de mais para menos e vice versa
+// const i_minus = document.querySelector("#i")
+
+//   const icone_mais = document.querySelector('#imais')
+// const icone_menos = document.querySelector("#")
+
+
+
+// criação das variaveis dos botões que se alterarão + pra menos 
--- js/script.js
+++ js/script.js
@@ -228,34 +228,16 @@ let tPartners = setInterval(()=>{
 
 },80
 );
- //  1- área education ok
- // 2 ->  colocar no js para remover classe de  a estilo ativo e ok
- // 2.1 pegar classe activo e remover ok
- // 2.2 trocar classe de icone dentro do link de fa-minus (menos) pra fa-plus (mais) ok
 
- // 3->  nao exibir conteudo p dentro do panel body  ok
- // 3.1 colocar ao evento anterior feito. cada link corresponde a uma caixa com dois paragrafos.  ok
- //3.2 -> verificar se visivel, então esconder conteudo, se não, então mostrar ok
-
-/// issue on toggle fa-minus to fa.plus - adiconar e substituiar classe  de elemento co m js
 const courses = document.querySelector(".panel-body")
-// const i_minus = document.querySelector('.fa-minus')
-// const i_plus = document.querySelector('.fa-plus')
+
 const btnSh = document.querySelector(".btn-show-hide")
 const btnSh2 = document.querySelector('.btn-show-hide2')
 const btnSh3 = document.querySelector('.btn-show-hide3')
 const panelBody = document.querySelector(".panel-body")
 const panelBody2 = document.querySelector(".panel-body2")
 const panelBody3 = document.querySelector(".panel-body3")
-    // toggle botao de mais para menos e vice versa
-// const i_minus = document.querySelector("#i")
-
-//   const icone_mais = document.querySelector('#imais')
-// const icone_menos = document.querySelector("#")
-
-
-
-// criação das variaveis dos botões que se alterarão + pra menos 
+   
 const minus = document.querySelector('#iMenos')
 
 let  el = document.createElement("i")
@@ -266,34 +248,13 @@ el.classList = "fa-solid fa-plus";
 
 btnSh.addEventListener("click",() => {
 if(panelBody.style.display === 'flex'){
-        /// issue on toggle fa-minus to fa.plus
+        
         panelBody.style.display = 'none';
         btnSh.classList.remove('active')
-// trocar icone para + ok 
+
 
         btnSh.appendChild(el,minus)
 
-        // minus.addEventListener("click",()=>{
-        // btnSh.appendChild(el,minus)
-        //     }
-        // )
-  
-            // )
-    //     if(btnSh.classList.contains('active')){
-          
-         
-
-    // }else{
-    //     el.addEventListener("click",()=>{
-    //    btnSh.classList.add('active')
-    //    btnSh.appendChild(minus,el)
-  
-    //  }
-    //  )
-    // }
-     
-
-// trocar icone para - ok 
 }else{
      panelBody.style.display = 'flex'
     btnSh.classList.add('active')
@@ -317,14 +278,7 @@ btnSh2.addEventListener("click",() => {
         
         
         
-        /// issue on toggle fa-minus to fa.plus
-        // if(btnSh2.classList.contains('active')){
-            // btnSh2.classList.remove('active')
-            // panelBody2.style.display = 'none';
-        // }else{
-        //  btnSh2.classList.add('active')
-
-        // }
+       
     }else{
         panelBody2.style.display = 'flex'
         btnSh2.classList.add('active')
@@ -363,51 +317,6 @@ btnSh2.addEventListener("click",() => {
         })
 
 
-    // if(btnSh.classList.contains('active')){
-    //     btnSh.classList.remove('active')
-    // }else{     /// issue on toggle fa-minus to fa.plus
-    //     btnSh.classList.add('active')
-    // }
-//     if(btnSh.classList.contains('.fa-minus')){
-//         btnSh.classList.remove('.fa-minus')
-//         btnSh.classList.add('.fa-plus')
-//     }else{
-//         btnSh.classList.remove('.fa-plus')
-//         btnSh.classList.add('.fa-minus')
-//     }
-// }
-
-// }else{
-//     panelBody.style.display = 'block';
-
-    // if(btnSh.classList.contains('.fa-minus')){
-    //     btnSh.classList.remove('.fa-minus')
-    //     btnSh.classList.add('.fa-plus')
-    // }else{
-    //     btnSh.classList.remove('.fa-plus')
-    //     btnSh.classList.add('.fa-minus')
-//     }
-
-// const showCourse = document.querySelector('.showCourse')
-// showCourse.addEventListener("click",() =>{
-//     if(tileCourse.classList.contains('.active')){
-//         tileCourse.classList.remove(".active")
-//     }else{
-//         tileCourse.classList.add(".active")
-//     }
-// })
-
-
-// const closeCourse() =>{
-//     const titleCourse1 = document.querySelector('.fa-minus')
-
-
-
-const changeLanguage = () => {
-    isEnglish = !isEnglish
-    document.querySelector('.about-me').innerHTML = isEnglish ? english.aboutMe : portuguese.aboutMe
-    document.querySelector('.about-me-text').innerHTML = isEnglish ? english.description : portuguese.description
-}
 
 const home = () => {
  document.querySelector('.about-me-text').innerHTML = "HI! i'M Yan. "
@@ -445,45 +354,7 @@ const portuguese = {
 const english = {
     aboutMe: 'About me...',
     description: 'I am Yan Isonel Pereira. Currently, I specialize in HTML | CSS | JAVASCRIPT (JS) | React.js and constantly strive to improve my skills. Therefore, I have experience with some technologies like AWS, Git and also GitHub.I am passionate about building successful products and work well in a team environment. I always seek to simplify processes to enhance the customer experience and efficiently solve project issues with front-end technology and I have in search to my first opportunity.'
-/* Amante da tecnologia desde 1999.  */
+
 }
 
 
-// fontes de orientação :
-
-//elementoPlataformas.classList.add('ativo')
-// botao.addEventListener.toggle(`ativo`)
-/*
-if(elementoPlataformas.classList.contains("ativo")){
-    elementoPlataformas.classList.remove('ativo')
-}else{
-    elementoPlataformas.classList.add('ativo')
-
-
-   */
-
-/*
-// botao para direita
-// const rightAction = () => {
-//     const $image = document.querySelector('.seletor-da-imagem')
-//     imagem.style = 'margin-left: 200px' // os pixels deve ser do tamanho da imagem
-//   }
-// lembrando, o css da div pai tem que ter o overflow hidden, para quando a imagem filha der a margin, a imagem que foi trocada sumir
-// tem varias formas de fazer
-// tem uma mais facil:
-
-// // botao para direita
-// const rightAction = () => {
-//   const $image = document.querySelector('.seletor-da-imagem')
-//   imagem.setAttribute('href', 'URL DA NOVA IMAGEM AQUI')
-// }
-// ai tu pode criar um array de links de imagens
-// const images = ['link 1', 'link 2', 'link 3']
-
-// // ISSO DEFINE QUAL IMAGEM ESTA SENDO USADA
-// let imageIndex = 0
-
-// setInterval(() => {
-//     // troca a imgem aqui
-// //  }, 5000)
-*/
